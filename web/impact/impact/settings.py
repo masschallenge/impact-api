7459062ae342d81b65eddaccd034d5d3120a7063
Nathan Wilson <nathan@masschallenge.org>
2017-06-28 00:16:40 +0000

    [AC-4501] Revised comment to refer to new ticket

--- web/impact/impact/settings.py
+++ web/impact/impact/settings.py
@@ -138,12 +138,10 @@ class Base(Configuration):
     V0_SITE_NAME = bytes(os.environ.get(
         'IMPACT_API_V0_SITE_NAME', 'masschallenge.org'), 'utf-8')
 
-    # This seems too complicated.  Why would we want to
-    # override the name of an API group using environment
-    # variables?  Shouldn't this name just be a constant?
     V0_API_GROUP = bytes(os.environ.get(
         'IMPACT_API_V0_API_GROUP', 'v0_clients'), 'utf-8')
 
+    # This and the above should get generalized.  See AC-4574.
     V1_API_GROUP = bytes(os.environ.get(
         'IMPACT_API_V1_API_GROUP', 'v1_clients'), 'utf-8')
 
