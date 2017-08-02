6d5b309f9ecb04ee1a9c6099d2313fe25d67f16a
Jon Kiparsky <jon@kiparsky.net>
2017-08-02 00:04:25 -0400

    [AC-4632] Code cleanup

--- web/impact/impact/models/organization.py
+++ web/impact/impact/models/organization.py
@@ -17,8 +17,7 @@ class Organization(MCModel):
     description = models.TextField(
         max_length=1000,
         blank=True,
-        help_text='This is the generic description of the Partner, shared '
-        'across all Programs.')
+        help_text='This is the generic description of the Organization.')
     website_url = models.URLField(max_length=100, blank=True)
     twitter_handle = models.CharField(
         max_length=40,
@@ -46,26 +45,3 @@ class Organization(MCModel):
     def __str__(self):
         return self.name
 
-    @classmethod
-    def slug_from_instance(cls, instance):
-        slug = slugify(instance.name)
-        if slug == "":
-            slug = "organization"
-        slug = slug[:61]
-        slugbase = slug
-        i = 0
-        while (cls.objects.filter(url_slug=slug).exists() and
-               (i < 100 or slugbase == "organization")):
-            i += 1
-            slug = slugbase + "-" + str(i)
-        return slug
-
-
-def organization_save_patch(self, *args, **kwargs):
-    if self.url_slug == "":
-        self.url_slug = Organization.slug_from_instance(self)
-    super(Organization, self).save_base(*args, **kwargs)
-
-
-Organization.save_base = organization_save_patch
-    
