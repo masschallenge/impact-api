50fac232624473520858bd1906c5ab92d16f3e35
Nathan Wilson <nathan@masschallenge.org>
2017-08-26 01:54:23 +0000

    [AC-4764] Tests for UserBecameFinalistEvent

--- web/impact/impact/tests/test_user_history_view.py
+++ web/impact/impact/tests/test_user_history_view.py
@@ -3,17 +3,20 @@
 
 from django.urls import reverse
 
+from impact.models import UserRole
 from impact.tests.factories import (
+    ProgramRoleGrantFactory,
     StartupTeamMemberFactory,
     UserFactory,
 )
-
 from impact.tests.api_test_case import APITestCase
 from impact.tests.utils import (
     days_from_now,
     find_events,
 )
+
 from impact.v1.events import (
+    UserBecameFinalistEvent,
     UserCreatedEvent,
     UserJoinedStartupEvent,
 )
@@ -57,3 +60,55 @@ class TestUserHistoryView(APITestCase):
             self.assertEqual(1, len(events))
             self.assertEqual(join_date, events[0]["datetime"])
             self.assertEqual(next_created_at, events[0]["latest_datetime"])
+
+    def test_user_became_finalist(self):
+        prg = ProgramRoleGrantFactory(
+            program_role__user_role__name=UserRole.FINALIST)
+        with self.login(username=self.basic_user().username):
+            url = reverse("user_history", args=[prg.person.id])
+            response = self.client.get(url)
+            events = find_events(response.data["history"],
+                                 UserBecameFinalistEvent.EVENT_TYPE)
+            self.assertEqual(1, len(events))
+            self.assertEqual(prg.created_at, events[0]["datetime"])
+
+    def test_user_became_finalist_no_created_at_new_user(self):
+        program_start_date = days_from_now(-4)
+        user_date = days_from_now(-2)
+        user = UserFactory(date_joined=user_date)
+        prg = ProgramRoleGrantFactory(
+            person=user,
+            program_role__program__start_date=program_start_date,
+            program_role__user_role__name=UserRole.FINALIST)
+        prg.created_at = None
+        prg.save()
+        next_prg_date = days_from_now(-1)
+        next_prg = ProgramRoleGrantFactory()
+        next_prg.created_at = next_prg_date
+        next_prg.save()
+        with self.login(username=self.basic_user().username):
+            url = reverse("user_history", args=[prg.person.id])
+            response = self.client.get(url)
+            events = find_events(response.data["history"],
+                                 UserBecameFinalistEvent.EVENT_TYPE)
+            self.assertEqual(1, len(events))
+            self.assertEqual(user_date, events[0]["datetime"])
+            self.assertEqual(next_prg_date, events[0]["latest_datetime"])
+
+    def test_user_became_finalist_no_created_at_old_user(self):
+        user_date = days_from_now(-4)
+        user = UserFactory(date_joined=user_date)
+        program_start_date = days_from_now(-2)
+        prg = ProgramRoleGrantFactory(
+            person=user,
+            program_role__program__start_date=program_start_date,
+            program_role__user_role__name=UserRole.FINALIST)
+        prg.created_at = None
+        prg.save()
+        with self.login(username=self.basic_user().username):
+            url = reverse("user_history", args=[prg.person.id])
+            response = self.client.get(url)
+            events = find_events(response.data["history"],
+                                 UserBecameFinalistEvent.EVENT_TYPE)
+            self.assertEqual(1, len(events))
+            self.assertEqual(program_start_date, events[0]["datetime"])
--- web/impact/impact/v1/events/user_became_finalist_event.py
+++ web/impact/impact/v1/events/user_became_finalist_event.py
@@ -1,9 +1,13 @@
-from impact.models import UserRole
+from datetime import datetime
+from impact.models import (
+    ProgramRoleGrant,
+    UserRole,
+)
 from impact.utils import compose_filter
 
 
 class UserBecameFinalistEvent(object):
-    DESCRIPTION_FORMAT = "Became Finalist of {}"
+    DESCRIPTION_FORMAT = "Became Finalist of {name} ({id})"
     EVENT_TYPE = "became finalist"
 
     def __init__(self, program_role_grant):
@@ -14,24 +18,34 @@ class UserBecameFinalistEvent(object):
         result = []
         for prg in user.programrolegrant_set.filter(**compose_filter(
             ["program_role",
-            "user_role",
-            "name"],
+             "user_role",
+             "name"],
             UserRole.FINALIST)):
             result.append(cls(prg))
         return result
 
     def serialize(self):
-        finalist_time = self._finalist_datetime()
+        earliest, latest = self._finalist_datetime()
+        program = self.program_role_grant.program_role.program
         return {
-            "datetime": finalist_time,
+            "datetime": earliest,
+            "latest_datetime": latest,
             "event_type": self.EVENT_TYPE,
-            "description": self.DESCRIPTION_FORMAT.format(
-                self.program_role_grant.program_role.program.name)
+            "description": self.DESCRIPTION_FORMAT.format(name=program.name,
+                                                          id=program.id)
             }
 
     def _finalist_datetime(self):
         result = self.program_role_grant.created_at
-        if result is None:
-            program = self.program_role_grant.program_role.program
-            return program.start_date
-        return result
+        if result:
+            return (result, result)
+        program = self.program_role_grant.program_role.program
+        earliest = max(program.start_date,
+                       self.program_role_grant.person.date_joined)
+        latest = datetime.now()
+        prg_with_created_at = ProgramRoleGrant.objects.filter(
+            id__gt=self.program_role_grant.id,
+            created_at__isnull=False).order_by("id").first()
+        if prg_with_created_at:
+            latest = prg_with_created_at.created_at
+        return (earliest, latest)
