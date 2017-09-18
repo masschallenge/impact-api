f7c2d091817baf8410930d94cc8cde202b5f808a
Nathan Wilson <nathan@masschallenge.org>
2017-09-18 19:02:36 +0000

    [AC-4835] Update to user/<id>/organizations with tests

--- web/impact/impact/v1/views/user_organizations_view.py
+++ web/impact/impact/v1/views/user_organizations_view.py
@@ -22,17 +22,34 @@ class UserOrganizationsView(LoggingMixin, APIView):
 
     def get(self, request, pk):
         self.instance = self.model.objects.get(pk=pk)
-        organizations = set(list(self.partner_organizations()) +
-                            list(self.startup_organizations()))
-        result = {"organizations": list(organizations)}
-        return Response(result)
+        data_by_org_id = {}
+        # Partner data wins over startup data.
+        all_data = self.startup_data() + self.partner_data()
+        for data in all_data:
+            id = data["id"]
+            org = data_by_org_id.get(id, {})
+            org.update(data)
+            data_by_org_id[id] = org
+        return Response({"organizations": data_by_org_id.values()})
 
-    def partner_organizations(self):
+    def partner_data(self):
+        # Oldest/lowest id first
         team = PartnerTeamMember.objects.filter(
-            team_member=self.instance)
-        return team.values_list('partner__organization_id', flat=True)
+            team_member=self.instance).order_by("partner__id")
+        data = team.values_list("partner__organization_id",
+                                "partner_administrator")
+        return [{"id": id,
+                 "partner_administrator": administrator}
+                for id, administrator in data]
 
-    def startup_organizations(self):
+    def startup_data(self):
+        # Oldest/lowest id first
         team = StartupTeamMember.objects.filter(
-            user=self.instance)
-        return team.values_list('startup__organization_id', flat=True)
+            user=self.instance).order_by("startup__id")
+        data = team.values_list("startup__organization_id",
+                                "startup_administrator",
+                                "primary_contact")
+        return [{"id": id,
+                 "startup_administrator": administrator,
+                 "primary_contact": primary_contact}
+                for id, administrator, primary_contact in data]
