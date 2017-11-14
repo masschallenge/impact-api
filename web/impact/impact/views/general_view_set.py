be82f1beac54f46fe9458c3ca26b8e3b786bc9f5
Sarah Dawson <sdawson@masschallenge.org>
2017-11-14 16:23:43 -0500

    [AC-5010] Create List of Model Attribute API Calls That Require Snake Case
    This commit creates a list of models that use attributes.  Those calls have snake case in their model definition, and were breaking when converted back to camel case.  This may not be the most efficient way to do this, but it is currently working.

--- web/impact/impact/views/general_view_set.py
+++ web/impact/impact/views/general_view_set.py
@@ -11,6 +11,21 @@ from impact.permissions import DynamicModelPermissions
 from impact.serializers import GeneralSerializer
 
 
+model_attribute_calls = [
+    'Startup_additional_industries',
+    'Startup_recommendation_tags',
+    'StartupLabel_startups',
+    'RefundCode_programs',
+    'UserLabel_users',
+    'Observer_newsletter_cc_roles',
+    'ExpertProfile_functional_expertise',
+    'ExpertProfile_additional_industries',
+    'ExpertProfile_mentoring_specialties',
+    'Newsletter_recipient_roles',
+    'Section_interest_categories',
+]
+
+
 class GeneralViewSet(LoggingMixin, viewsets.ModelViewSet):
     permission_classes = (
         permissions.IsAuthenticated,
@@ -19,9 +34,15 @@ class GeneralViewSet(LoggingMixin, viewsets.ModelViewSet):
 
     @property
     def model(self):
-        return apps.get_model(
-            app_label=self.kwargs['app'],
-            model_name=snake_to_camel_case(self.kwargs['model']))
+        if self.kwargs['model'] in model_attribute_calls:
+            return apps.get_model(
+                app_label=self.kwargs['app'],
+                model_name=self.kwargs['model'])
+        else:
+            return apps.get_model(
+                app_label=self.kwargs['app'],
+                model_name=snake_to_camel_case(self.kwargs['model']))
+
 
     def get_queryset(self):
         return self.model.objects.all()
