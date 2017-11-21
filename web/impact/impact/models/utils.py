3e2bbb2dc3034c707f2253fa997c8c54e5a6a1bf
Sarah Dawson <sdawson@masschallenge.org>
2017-11-21 16:47:41 -0500

    [AC-5010] Update Case Change To Use Django's Builtin Utility

--- web/impact/impact/models/utils.py
+++ web/impact/impact/models/utils.py
@@ -19,3 +19,17 @@ def model_has_field(model, field_name):
         return True
     except FieldDoesNotExist:
         return False
+
+
+def model_name_to_snake(value):
+    original_model_string = camel_case_to_spaces(value)
+    new_model_string = original_model_string.replace(" ", "_")
+    return new_model_string
+ 
+
+def snake_to_model_name(value):
+    old_value = value.split('_')
+    new_value = ""
+    for word in old_value:
+        new_value += (word[0].upper() + word[1:])
+    return new_value
