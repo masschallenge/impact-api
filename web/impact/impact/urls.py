ded9520b64f096ce5e457779b9d048dd5854269d
Yotam Manor <yotammanor@gmail.com>
2017-12-20 17:30:37 -0500

    [AC-5111] sort jwt urlpatterns

--- web/impact/impact/urls.py
+++ web/impact/impact/urls.py
@@ -153,8 +153,8 @@ v1_urlpatterns = [
 
 jwt_urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
+    url(r'^api-token-verify/', verify_jwt_token),
 ]
 
 urls = [
