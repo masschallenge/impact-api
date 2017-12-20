a7ebffc0461e726cab8c5568bb58b36110ce4cac
Nathan Wilson <nathan@masschallenge.org>
2017-12-20 17:53:37 +0000

    [AC-5122] Removed redundant call to get_wsgi_application()

--- web/impact/impact/wsgi.py
+++ web/impact/impact/wsgi.py
@@ -11,7 +11,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'impact.settings')
 # environ must be set before importing jazzband/django-configurations
 from configurations.wsgi import get_wsgi_application  # noqa: E402
 
-application = get_wsgi_application()
 newrelic_application = newrelic.agent.wsgi_application()
 application = newrelic_application(get_wsgi_application())
 newrelic.agent.initialize(
