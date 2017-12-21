f9df72b2dd61dc54ca3c9636a2d06b8ebb8e224e
Yotam Manor <yotammanor@gmail.com>
2017-12-21 14:57:24 -0500

    [AC-5111] rename jwt urls

--- web/impact/impact/urls.py
+++ web/impact/impact/urls.py
@@ -39,9 +39,9 @@ for model in apps.get_models('accelerator'):
             base_name="accelerator.{model}".format(model=model.__name__))
 
 sso_urlpatterns = [
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api-token-verify/', verify_jwt_token),
+    url(r'^obtain-token/', obtain_jwt_token),
+    url(r'^refresh-token/', refresh_jwt_token),
+    url(r'^verify-token/', verify_jwt_token),
 ]
 
 account_urlpatterns = [
