9600d4b921580c4f18419e95e50697287e180e78
Nathan Wilson <nathan@masschallenge.org>
2018-02-07 19:39:34 +0000

    [AC-5291] Rolling back to more parallel code and tell CodeClimate it is ok

--- web/impact/impact/v1/helpers/program_helper.py
+++ web/impact/impact/v1/helpers/program_helper.py
@@ -30,6 +30,10 @@ PROGRAM_FIELDS = {
 class ProgramHelper(ModelHelper):
     model = Program
 
+    @classmethod
+    def fields(cls):
+        return PROGRAM_FIELDS
+
     @property
     def cycle_name(self):
         return self.field_element("cycle", "name")
@@ -37,7 +41,3 @@ class ProgramHelper(ModelHelper):
     @property
     def program_family_name(self):
         return self.field_element("program_family", "name")
-
-    @classmethod
-    def fields(cls):
-        return PROGRAM_FIELDS
