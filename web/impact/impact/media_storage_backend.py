30daab8d849592ff6836af537c0795810d11254b
Yotam Manor <yotammanor@gmail.com>
2018-03-05 20:53:36 +0200

    [AC-3838] add Boto3 Storage in prod settings, keep regular in dev/test.

---
+++ web/impact/impact/media_storage_backend.py
@@ -0,0 +1,10 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class MediaStorageBackend(S3Boto3Storage):
+    custom_domain = False
+    default_acl = 'private'
+    file_overwrite = False
+
+    def path(self, name):
+        return None
