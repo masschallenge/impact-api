dafc8bc68ed8a95813fe216cd8cc88aca8e043b1
Shankar Ambady <shanbady@gmail.com>
2018-03-19 12:06:08 -0400

    pushing tags

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,6 @@
 semantic-release --major version
 export TAG=$(semantic-release version --noop | grep "Current version: " | cut -d ' ' -f 3)
 git push
+git push --tags
 cd ../django-accelerator && git tag $TAG && git push --tags 
 cd ../accelerate && git tag $TAG && git push --tags
