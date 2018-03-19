d8ff247872432318ce16dc9a3c1cd8dbe0fcf48c
Shankar Ambady <shanbady@gmail.com>
2018-03-19 11:46:50 -0400

    making sure tagged commit is pushed

--- create_release.sh
+++ create_release.sh
@@ -1,4 +1,5 @@
 semantic-release --major version
 export TAG=$(semantic-release version --noop | grep "Current version: " | cut -d ' ' -f 3)
+git push
 cd ../django-accelerator && git tag $TAG && git push --tags 
 cd ../accelerate && git tag $TAG && git push --tags
