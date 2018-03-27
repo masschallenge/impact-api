0b21b0c95f5dbb2eaedb9d8d2be3f202c80d60a4
Yotam Manor <yotammanor@gmail.com>
2018-03-27 18:38:42 +0300

    [AC-5347] add back the start.sh migration workaround.

--- web/scripts/start.sh
+++ web/scripts/start.sh
@@ -7,6 +7,12 @@ if [ "${DJANGO_CONFIGURATION}" == "Dev" ]; then
         echo "..."
   done
 fi
+PDT_MIGRATION_APPLIED=$(python3 manage.py showmigrations pdt | grep 0002 | grep X);
+if [[ -z $PDT_MIGRATION_APPLIED ]];
+then
+  python3 manage.py migrate pdt 0002 --fake --noinput;
+fi
+# todo: remove this conditional after the transition is over
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 service nginx restart
