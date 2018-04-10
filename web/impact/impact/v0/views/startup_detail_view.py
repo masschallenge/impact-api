5613dcd5a399b050c55240124bb16f08051d4ec1
Yotam Manor <yotammanor@gmail.com>
2018-04-10 14:23:05 +0300

    [AC-5382] re-sort imports in startup_detail_view.

--- web/impact/impact/v0/views/startup_detail_view.py
+++ web/impact/impact/v0/views/startup_detail_view.py
@@ -10,10 +10,10 @@ from embed_video.backends import (
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from accelerator.models import ProgramStartupStatus
 from impact.permissions import (
     V0APIPermissions,
 )
-from accelerator.models import ProgramStartupStatus
 from impact.utils import get_profile
 from impact.v0.api_data.startup_detail_data import StartupDetailData
 from impact.v0.views.utils import (
