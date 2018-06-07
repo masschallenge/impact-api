1de4cac0fec8093d4e8b0f0c0cb723b555ad2692
Nathan Wilson <nathan@masschallenge.org>
2018-06-07 22:07:02 +0000

    [AC-5679] Fix cors test

--- web/impact/impact/tests/test_cors_setup.py
+++ web/impact/impact/tests/test_cors_setup.py
@@ -2,11 +2,13 @@
 # Copyright (c) 2017 MassChallenge, Inc.
 
 from django.test import TestCase
+from django.conf import settings
 
 
 class TestCorsSetup(TestCase):
 
     def test_expose_headers(self):
-        resp = self.client.get('/', HTTP_ORIGIN='http://thirdparty.com')
+        local_url = "http://" + settings.CORS_ORIGIN_WHITELIST[0]
+        resp = self.client.get('/', HTTP_ORIGIN=local_url)
         self.assertEqual(
             resp['access-control-allow-credentials'], 'true')
