e706335e235843b09ab1998621649d74bf63ef73
Yotam Manor <yotammanor@gmail.com>
2018-06-14 21:25:17 +0300

    [AC-5723] rewrite the mechanism for algolia filters

--- web/impact/impact/views/algolia_api_key_view.py
+++ web/impact/impact/views/algolia_api_key_view.py
@@ -9,8 +9,19 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from accelerator.models import UserRole
-from accelerator_abstract.models import CURRENT_STATUSES
+from accelerator.models import (
+    Program,
+    ProgramFamily,
+    ProgramRole,
+    UserRole,
+)
+from accelerator_abstract.models import (
+    ACTIVE_PROGRAM_STATUS,
+    ENDED_PROGRAM_STATUS,
+)
+
+IS_CONFIRMED_MENTOR_FILTER = "is_confirmed_mentor:true"
+CONFIRMED_MENTOR_IN_PROGRAM_FILTER = 'confirmed_mentor_programs:"{program}"'
 
 
 class AlgoliaApiKeyView(APIView):
@@ -48,20 +59,31 @@ def _get_search_key(request):
 def _get_filters(request):
     if request.user.is_staff:
         return []
-    active_roles = UserRole.FINALIST_USER_ROLES
-    active_roles.append(UserRole.MENTOR)
+    participant_roles = UserRole.FINALIST_USER_ROLES
+    participant_roles.append(UserRole.MENTOR)
+    user_program_roles_as_participant = ProgramRole.objects.filter(
+        programrolegrant__person=request.user,
+        user_role__name__in=participant_roles
+    )
+    program_groups = Program.objects.filter(
+        programrole__in=user_program_roles_as_participant).values_list(
+        'mentor_program_group', flat=True).distinct()
+    program_families = ProgramFamily.objects.filter(
+        programs__mentor_program_group__in=program_groups).prefetch_related(
+        'programs').distinct()
+
     facet_filters = []
-    for grant in request.user.programrolegrant_set.filter(
-            program_role__program__program_status__in=CURRENT_STATUSES,
-            program_role__user_role__name__in=active_roles
-    ):
-        facet_filters.append(
-            'confirmed_mentor_programs:"{active_program}"'.format(
-                active_program=grant.program_role.program.name))
+    for program_family in program_families:
+        past_or_present_programs = program_family.programs.filter(
+            program_status__in=(ACTIVE_PROGRAM_STATUS, ENDED_PROGRAM_STATUS)
+        ).order_by('-start_date')
+        if past_or_present_programs:
+            facet_filters.append(CONFIRMED_MENTOR_IN_PROGRAM_FILTER.format(
+                program=past_or_present_programs.first().name))
     if len(facet_filters) > 0:
         return " OR ".join(facet_filters)
     else:
-        return "is_confirmed_mentor:true"
+        return IS_CONFIRMED_MENTOR_FILTER
 
 
 def _get_public_key(params, search_key):
