e3628090fc961ce0da4bfbedf601c109afcce0bf
Yotam Manor <yotammanor@gmail.com>
2018-06-14 19:08:29 +0300

    [AC-5723] cleanup code for algolia_api_key_view.

--- web/impact/impact/views/algolia_api_key_view.py
+++ web/impact/impact/views/algolia_api_key_view.py
@@ -1,31 +1,16 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import permissions
-from algoliasearch import algoliasearch
-from django.conf import settings
-from accelerator_abstract.models import CURRENT_STATUSES
-from accelerator.models import UserRole
 import time
 
+from algoliasearch import algoliasearch
+from django.conf import settings
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-def _get_user_filters(request):
-        active_roles = UserRole.FINALIST_USER_ROLES
-        active_roles.append(UserRole.MENTOR)
-        facet_filters = []
-        for grant in request.user.programrolegrant_set.filter(
-                program_role__program__program_status__in=CURRENT_STATUSES,
-                program_role__user_role__name__in=active_roles
-        ):
-            facet_filters.append(
-                'confirmed_mentor_programs:"{active_program}"'.format(
-                    active_program=grant.program_role.program.name))
-        if len(facet_filters) > 0:
-            return " OR ".join(facet_filters)
-        else:
-            return "is_confirmed_mentor:true"
+from accelerator.models import UserRole
+from accelerator_abstract.models import CURRENT_STATUSES
 
 
 class AlgoliaApiKeyView(APIView):
@@ -38,25 +23,50 @@ class AlgoliaApiKeyView(APIView):
     actions = ["GET"]
 
     def get(self, request, format=None):
-        index_prefix = settings.ALGOLIA_INDEX_PREFIX
-        client = algoliasearch.Client(
-            settings.ALGOLIA_APPLICATION_ID,
-            settings.ALGOLIA_API_KEY)
+        search_key = _get_search_key(request)
+        filters = _get_filters(request)
         params = {
             'hitsPerPage': 24,
             'validUntil': int(time.time()) + 3600,
-            'userToken': request.user.id
+            'userToken': request.user.id,
+            'filters': filters
         }
-        search_key = settings.ALGOLIA_SEARCH_ONLY_API_KEY
-        if request.user.is_staff:
-            search_key = settings.ALGOLIA_STAFF_SEARCH_ONLY_API_KEY
-        else:
-            params['filters'] = _get_user_filters(request)
-        public_key = client.generateSecuredApiKey(
-            search_key,
-            params)
-
+        public_key = _get_public_key(params, search_key)
         return Response({
             'token': public_key,
-            'index_prefix': index_prefix,
-            'filters': params.get('filters', [])})
+            'index_prefix': settings.ALGOLIA_INDEX_PREFIX,
+            'filters': filters
+        })
+
+
+def _get_search_key(request):
+    if request.user.is_staff:
+        return settings.ALGOLIA_STAFF_SEARCH_ONLY_API_KEY
+    return settings.ALGOLIA_SEARCH_ONLY_API_KEY
+
+
+def _get_filters(request):
+    if request.user.is_staff:
+        return []
+    active_roles = UserRole.FINALIST_USER_ROLES
+    active_roles.append(UserRole.MENTOR)
+    facet_filters = []
+    for grant in request.user.programrolegrant_set.filter(
+            program_role__program__program_status__in=CURRENT_STATUSES,
+            program_role__user_role__name__in=active_roles
+    ):
+        facet_filters.append(
+            'confirmed_mentor_programs:"{active_program}"'.format(
+                active_program=grant.program_role.program.name))
+    if len(facet_filters) > 0:
+        return " OR ".join(facet_filters)
+    else:
+        return "is_confirmed_mentor:true"
+
+
+def _get_public_key(params, search_key):
+    client = algoliasearch.Client(
+        settings.ALGOLIA_APPLICATION_ID,
+        settings.ALGOLIA_API_KEY)
+    public_key = client.generateSecuredApiKey(search_key, params)
+    return public_key
