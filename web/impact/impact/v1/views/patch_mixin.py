80f5aaaa76bdf7266c41d562fb8128014791f5b0
Jon Kiparsky <jon@kiparsky.net>
2018-06-19 14:28:31 -0400

    [AC-5622] Add patch capability for option specs

--- web/impact/impact/v1/views/patch_mixin.py
+++ web/impact/impact/v1/views/patch_mixin.py
@@ -5,6 +5,7 @@ REQUIRED_KEY_ERROR = "'{}' is required"
 INVALID_KEY_ERROR = "'{}' is not a valid key."
 NO_OBJECT_ERROR = "Unable to find object for id {}"
 
+
 class PatchMixin(object):
     def patch(self, request, pk):
         object = self.helper_class.model.objects.filter(pk=pk).first()
@@ -20,20 +21,21 @@ class PatchMixin(object):
         return Response(status=204)
 
     def error_response(self, keys, helper):
-        note = valid_keys_note(keys.intersection(set(self.helper_class.INPUT_KEYS)))
+        note = valid_keys_note(keys.intersection(
+            set(self.helper_class.INPUT_KEYS)))
         return Response(status=403, data=helper.errors + [note])
-                       
+
     def set_fields(self, data, helper):
         for key, value in data.items():
             helper.field_setter(key, value)
         helper.save()
-        
+
     def validate_keys(self, keys, data, helper):
         result = {}
         for key in keys.intersection(self.helper_class.INPUT_KEYS):
             result[key] = helper.validate(key, data[key])
         return result
-    
+
     def _invalid_keys(self, keys):
         for key in set(keys) - set(self.helper_class.INPUT_KEYS):
             self.errors.append(INVALID_KEY_ERROR.format(key))
