8394011dc2cd0a6fe682c435b8e09f8accb1311f
Jon Kiparsky <jon@kiparsky.net>
2018-06-21 16:33:43 -0400

    [AC-5622] Move some code around because code climate said so

--- web/impact/impact/v1/views/criterion_option_spec_list_view.py
+++ web/impact/impact/v1/views/criterion_option_spec_list_view.py
@@ -1,12 +1,13 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
 
-from impact.v1.views.base_list_view import BaseListView
 from impact.v1.views.post_mixin import PostMixin
+from impact.v1.views.base_list_view import BaseListView
 from impact.v1.helpers import CriterionOptionSpecHelper
 
 
-class CriterionOptionSpecListView(BaseListView, PostMixin):
+class CriterionOptionSpecListView(BaseListView,
+                                  PostMixin):
+    helper_class = CriterionOptionSpecHelper    
     view_name = "criterion_option_spec"
-    helper_class = CriterionOptionSpecHelper
     actions = ['GET', 'POST']  # Should get this from PostMixin
