7be318e5bf630a4c7dc11fc696ed950ffa6da9e3
Jon Kiparsky <jon@kiparsky.net>
2018-06-21 16:35:24 -0400

    [AC-5622] More monkey moving

--- web/impact/impact/v1/views/criterion_detail_view.py
+++ web/impact/impact/v1/views/criterion_detail_view.py
@@ -1,12 +1,13 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
 
-from impact.v1.views.base_detail_view import BaseDetailView
 from impact.v1.views.patch_mixin import PatchMixin
+from impact.v1.views.base_detail_view import BaseDetailView
 from impact.v1.helpers import CriterionHelper
 
 
-class CriterionDetailView(BaseDetailView, PatchMixin):
-    actions = ["GET", "PATCH"]
+class CriterionDetailView(BaseDetailView,
+                          PatchMixin):
     view_name = "criterion"
     helper_class = CriterionHelper
+    actions = ["GET", "PATCH"]
