e49871b01e19ffda7388619da38391750c367ca9
Yotam Manor <yotammanor@gmail.com>
2018-07-03 12:13:49 +0300

    [AC-5666] add program interests field to ExpertProfileType

--- web/impact/impact/graphql/types/expert_profile_type.py
+++ web/impact/impact/graphql/types/expert_profile_type.py
@@ -7,12 +7,15 @@ from impact.graphql.types.startup_type import StartupType
 from impact.graphql.types.program_family_type import ProgramFamilyType  # noqa: F401, E501
 from impact.graphql.types.user_type import UserType  # noqa: F401
 from impact.graphql.types.functional_expertise_type import (
-    FunctionalExpertiseType  # noqa: F401, E501
+    FunctionalExpertiseType  # noqa: F401
 )
+from impact.graphql.types.interest_category_type import InterestCategoryType  # noqa: F401, E501
+
 
 class ExpertProfileType(DjangoObjectType):
     mentees = graphene.List(StartupType)
     image_url = graphene.String()
+    program_interests = graphene.List(graphene.String)
 
     class Meta:
         model = ExpertProfile
@@ -30,7 +33,12 @@ class ExpertProfileType(DjangoObjectType):
             'home_program_family',
             'user',
             'functional_expertise',
+            'interest_categories',
         )
 
     def resolve_image_url(self, info, **kwargs):
         return self.image and self.image.url
+
+    def resolve_program_interests(self, info, **kwargs):
+        return self.interest_categories.all().values_list(
+            'program__program_family__name', flat=True).distinct()
