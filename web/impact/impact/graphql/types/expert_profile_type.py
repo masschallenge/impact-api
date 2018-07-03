47747415a35c6d3c682cf832833bf356bbb2c6ec
Yotam Manor <yotammanor@gmail.com>
2018-07-03 17:59:34 +0300

    [AC-5666] add available_office_hours field on ExpertProfileType.

--- web/impact/impact/graphql/types/expert_profile_type.py
+++ web/impact/impact/graphql/types/expert_profile_type.py
@@ -1,7 +1,12 @@
 import graphene
 from graphene_django import DjangoObjectType
-
-from accelerator.models.expert_profile import ExpertProfile
+from django.utils import timezone
+from accelerator.models import (
+    ExpertProfile,
+    UserRole,
+    ProgramRole,
+    Program,
+)
 from impact.graphql.types.industry_type import IndustryType  # noqa: F401
 from impact.graphql.types.startup_type import StartupType
 from impact.graphql.types.program_family_type import ProgramFamilyType  # noqa: F401, E501
@@ -16,6 +21,7 @@ class ExpertProfileType(DjangoObjectType):
     mentees = graphene.List(StartupType)
     image_url = graphene.String()
     program_interests = graphene.List(graphene.String)
+    available_office_hours = graphene.Boolean()
 
     class Meta:
         model = ExpertProfile
@@ -42,3 +48,27 @@ class ExpertProfileType(DjangoObjectType):
     def resolve_program_interests(self, info, **kwargs):
         return self.interest_categories.all().values_list(
             'program__program_family__name', flat=True).distinct()
+
+    def resolve_available_office_hours(self, info, **kwargs):
+        user = info.context.user
+        filter_kwargs = {
+            'finalist__isnull': True,
+            'date__gte': timezone.now(),
+            'start_time__gte': timezone.now().time()
+        }
+        if not user.is_staff:
+            filter_kwargs['program__in'] = get_user_programs(user)
+        return self.user.mentor_officehours.filter(
+            **filter_kwargs).exists()
+
+
+def get_user_programs(user):
+    # todo: refactor this and move it to a sensible place
+    # todo: test this
+    participant_roles = UserRole.FINALIST_USER_ROLES
+    user_program_roles_as_participant = ProgramRole.objects.filter(
+        programrolegrant__person=user,
+        user_role__name__in=participant_roles
+    )
+    return Program.objects.filter(
+        programrole__in=user_program_roles_as_participant).distinct()
