5e80122c20e04b9208f6bc4ce13bc7ab7f757ff8
Nathan Wilson <nathan@masschallenge.org>
2018-07-06 18:11:04 +0000

    [AC-5625] Remove dead code and minor refactor

--- web/impact/impact/v1/helpers/matching_criterion_helper.py
+++ web/impact/impact/v1/helpers/matching_criterion_helper.py
@@ -14,12 +14,6 @@ class MatchingCriterionHelper(CriterionHelper):
     def app_count(self, apps, option_name):
         return self.target_counts(apps).get(option_name, 0)
 
-    def refine_feedbacks(self, feedbacks, target, refinement):
-        if not target:
-            return None
-        query = Q(**{refinement: target})
-        return feedbacks.filter(query)
-
     def find_app_ids(self, feedbacks, apps, target):
         if not feedbacks:
             return []
@@ -30,11 +24,13 @@ class MatchingCriterionHelper(CriterionHelper):
                 if app_id in app_map and app_map[app_id] == target.id]
 
     def app_ids_to_targets(self, apps):
-        if not self._app_ids_to_targets:
-            self.calc_app_ids_to_targets(apps)
+        self._check_cache(apps)
         return self._app_ids_to_targets
 
     def target_counts(self, apps):
+        self._check_cache(apps)
+        return self._target_counts
+
+    def _check_cache(self, apps):
         if not self._app_ids_to_targets:
             self.calc_app_ids_to_targets(apps)
-        return self._target_counts
