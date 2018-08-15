0c941bfef359eb59770386bf0d85ff299c50e453
mkiterian <hmukite@gmail.com>
2018-08-15 20:02:37 +0300

    [AC-5889] handle for user ids that are not for experts

--- web/impact/impact/graphql/query.py
+++ web/impact/impact/graphql/query.py
@@ -1,4 +1,5 @@
 import graphene
+from django.core.exceptions import ObjectDoesNotExist
 
 from impact.graphql.types.expert_profile_type import ExpertProfileType
 from accelerator.models.expert_profile import ExpertProfile
@@ -10,4 +11,8 @@ class Query(graphene.ObjectType):
     def resolve_expert_profile(self, info, **kwargs):
         user_id = kwargs.get('id')
 
-        return ExpertProfile.objects.get(user_id=user_id) if user_id else None
+        try:
+            return (ExpertProfile.objects.get(user_id=user_id)
+                    if user_id else None)
+        except ObjectDoesNotExist:
+            return None
