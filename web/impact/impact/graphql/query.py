78c8a33251e793ab937271cd0765692ad11fe890
mkiterian <hmukite@gmail.com>
2018-08-15 22:28:13 +0300

    [AC-5889] Query User model before querying ExpertProfile model

--- web/impact/impact/graphql/query.py
+++ web/impact/impact/graphql/query.py
@@ -1,8 +1,12 @@
 import graphene
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 
 from impact.graphql.types.expert_profile_type import ExpertProfileType
 from accelerator.models.expert_profile import ExpertProfile
+from accelerator_abstract.models.base_base_profile import (
+    EXPERT_USER_TYPE,
+)
 
 
 class Query(graphene.ObjectType):
@@ -11,8 +15,13 @@ class Query(graphene.ObjectType):
     def resolve_expert_profile(self, info, **kwargs):
         user_id = kwargs.get('id')
 
+        User = get_user_model()
+
         try:
-            return (ExpertProfile.objects.get(user_id=user_id)
-                    if user_id else None)
+            user = User.objects.get(id=user_id)
+            if user.get_profile().user_type.upper() == EXPERT_USER_TYPE:
+                return ExpertProfile.objects.get(user_id=user_id)
         except ObjectDoesNotExist:
             return None
+
+        return None
