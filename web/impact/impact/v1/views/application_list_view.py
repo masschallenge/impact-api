ee27b0d195a367d203dc80597aa98ae363ab8a4d
Paul Bissex <paul@masschallenge.org>
2018-09-11 16:54:54 -0400

    [AC-5961] fixed sorting of imports in view

--- web/impact/impact/v1/views/application_list_view.py
+++ web/impact/impact/v1/views/application_list_view.py
@@ -1,8 +1,8 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
 
-from impact.v1.views.base_list_view import BaseListView
 from impact.v1.helpers import ApplicationHelper
+from impact.v1.views.base_list_view import BaseListView
 
 
 class ApplicationListView(BaseListView):
