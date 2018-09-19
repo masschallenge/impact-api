a9037e3cd862e96e54c84fbe7ea189fcc871e0f6
Paul Bissex <paul@masschallenge.org>
2018-09-19 14:14:19 -0400

    [AC-5993] corrected method name

--- web/impact/impact/tests/test_clearance.py
+++ web/impact/impact/tests/test_clearance.py
@@ -15,19 +15,19 @@ class TestClearanceOrdering(APITestCase):
     def test_higher_clearance_accepted(self):
         clearance = ClearanceFactory(level=CLEARANCE_LEVEL_GLOBAL_MANAGER)
         user = clearance.user
-        cleared = Clearance.objects.has_clearance(user, CLEARANCE_LEVEL_POM)
+        cleared = Clearance.objects.check_clearance(user, CLEARANCE_LEVEL_POM)
         self.assertTrue(cleared)
 
     def test_equal_clearance_accepted(self):
         clearance = ClearanceFactory(level=CLEARANCE_LEVEL_GLOBAL_MANAGER)
         user = clearance.user
-        cleared = Clearance.objects.has_clearance(
+        cleared = Clearance.objects.check_clearance(
             user, CLEARANCE_LEVEL_GLOBAL_MANAGER)
         self.assertTrue(cleared)
 
     def test_lower_clearance_rejected(self):
         clearance = ClearanceFactory(level=CLEARANCE_LEVEL_POM)
         user = clearance.user
-        cleared = Clearance.objects.has_clearance(
+        cleared = Clearance.objects.check_clearance(
             user, CLEARANCE_LEVEL_GLOBAL_MANAGER)
         self.assertFalse(cleared)
