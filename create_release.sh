c7426d35967d4679fdc1d37cfc6fed102641e531
Vincent Asante <vincenthokie@gmail.com>
2019-01-22 16:51:02 +0300

    [AC-6257] use lowercase rather than uppercase argument to the 'make release' command

--- create_release.sh
+++ create_release.sh
@@ -1,14 +1,14 @@
 
 # if a tag hasn't been passed in as a parameter, create a semantic one
-if [ ! -n "$TAG" ]; then
+if [ ! -n "$tag" ]; then
     docker run --rm  -v"$(pwd)":/app  -ti semantic-release  -- semantic-release version
     export pwd=$(pwd)
     export TAG=$(docker run --tty=false --rm  -v$pwd:/app  -i semantic-release -- semantic-release version --noop | grep "Current version: " | cut -d ' ' -f 3 | sed -e "s/\r//")
 else
-    echo "You passed in '$TAG' as the custom tag"
+    echo "You passed in '$tag' as the custom tag"
     # TR == test release
-    echo "We're going to refactor the tag into 'TR-$TAG' to avoid any conflicts."
-    TAG="TR-$TAG"
+    echo "We're going to refactor the tag into 'TR-$tag' to avoid any conflicts."
+    TAG="TR-$tag"
     git tag "v${TAG}"
 fi
 
