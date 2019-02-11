c0c50552f3c9d2647b1e0009ba6f4125a3fce752
Vincent Asante <vincenthokie@gmail.com>
2019-02-11 15:56:47 +0300

    [development] centralize imports

---
+++ web/impact/impact/graphql/types/__init__.py
@@ -0,0 +1,19 @@
+from impact.graphql.types.user_type import UserType
+from impact.graphql.types.program_type import ProgramType
+from impact.graphql.types.startup_type import StartupType
+from impact.graphql.types.industry_type import IndustryType
+from impact.graphql.types.program_family_type import ProgramFamilyType
+from impact.graphql.types.interest_category_type import InterestCategoryType
+from impact.graphql.types.startup_team_member_type import StartupTeamMemberType
+from impact.graphql.types.entrepreneur_profile_type import (
+    EntrepreneurProfileType
+)
+from impact.graphql.types.functional_expertise_type import (
+    FunctionalExpertiseType
+)
+from impact.graphql.types.entrepreneur_profile_type import (
+    EntrepreneurProfileType
+)
+from impact.graphql.types.startup_mentor_relationship_type import (
+    StartupMentorRelationshipType
+)
