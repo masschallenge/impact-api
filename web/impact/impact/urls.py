37d6bc5ea378847bf4e029fd4d97863b5beb7987
Vincent Asante <vincenthokie@gmail.com>
2019-06-24 18:34:11 +0300

    [AC-1] update startup directory path from '/startup' to '/startups'

--- web/impact/impact/urls.py
+++ web/impact/impact/urls.py
@@ -123,7 +123,7 @@ urls = [
     url(r'^people/(.*)/$', TemplateView.as_view(
         template_name='front-end.html'),
         name="entreprenuer_profile"),
-    url(r'^startup/$', TemplateView.as_view(
+    url(r'^startups/$', TemplateView.as_view(
         template_name='front-end.html'),
         name="startup_profile"),
     url(r'^openid/', include('oidc_provider.urls', namespace='oidc_provider')),
