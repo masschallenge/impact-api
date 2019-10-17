4b5f952a20ffc1f51dc73240e5fa3e2b4a90f0ce
Kevin M Quigley <kquigley@masschallenge.org>
2019-10-17 16:25:33 -0400

    [AC-7058] Create test class

---
+++ web/impact/impact/tests/test_track_api_calls.py
@@ -0,0 +1,13 @@
+from django.test import (
+    RequestFactory,
+    TestCase,
+)
+
+from mock import patch
+
+class TestTrackAPICalls(TestCase):
+    def test_when_user(self):
+        pass
+
+    def test_when_no_user(self):
+        pass
