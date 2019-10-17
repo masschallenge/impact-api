e51ad75fa6780d8fa14300669797ae970beccec1
Kevin M Quigley <kquigley@masschallenge.org>
2019-10-17 14:15:31 -0400

    [AC-7058] first pass api tracking

---
+++ web/impact/impact/middleware/track_api_calls.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(__file__)
+
+class TrackAPICalls(object):
+    def process_request(self, request):
+        user = getattr(request, 'user', None)
+        if user:
+            user_email = user.email
+        else:
+            user_email = None
+
+        data = {
+            'user': user_email,
+            'path': request.path,
+            'uri': request.build_absolute_uri,
+            'is_ajax': request.is_ajax(),
+        }
+        logger.info(data)
+        return
