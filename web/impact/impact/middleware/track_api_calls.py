1552a3813dc434b0b8ebf6c6198861c627ab3d06
Kevin M Quigley <kquigley@masschallenge.org>
2019-10-18 15:40:31 -0400

    [AC-7058] fix user email attr getter

--- web/impact/impact/middleware/track_api_calls.py
+++ web/impact/impact/middleware/track_api_calls.py
@@ -5,11 +5,8 @@ logger = logging.getLogger(__file__)
 
 class TrackAPICalls(object):
     def process_request(self, request):
-        user = getattr(request, 'user', None)
-        if user:
-            user_email = user.email
-        else:
-            user_email = None
+        user = request.user
+        user_email = getattr(user, 'email', None)
 
         data = {
             'user': user_email,
