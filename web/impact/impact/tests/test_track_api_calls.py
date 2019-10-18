e0c32f8fcafdd1d951a14bf16111e1aa4f25e11c
Kevin M Quigley <kquigley@masschallenge.org>
2019-10-18 17:21:40 -0400

    [AC-7058] Fix test

--- web/impact/impact/tests/test_track_api_calls.py
+++ web/impact/impact/tests/test_track_api_calls.py
@@ -4,11 +4,19 @@ from impact.tests.api_test_case import APITestCase
 
 
 class TestTrackAPICalls(APITestCase):
-    @patch('middleware.track_api_calls.TrackAPICalls.process_request.logger')
-    def test_when_user_authenticated(self, logger_info_patch):
-        with self.login(email=self.basic_user().email):
+    @patch('impact.middleware.track_api_calls.logger')
+    def test_when_user_authenticated(self, logger_patch):
+        email = self.basic_user().email
+        expected = {
+            'user': email,
+            'path': '/',
+            'uri': 'http://testserver/',
+            'is_ajax': False,
+        }
+
+        with self.login(email=email):
             self.client.get('/')
-            logger_info_patch.info.assert_called_with({})
+            logger_patch.info.assert_called_with(expected)
 
     def test_when_no_user_authenticated(self):
         pass
