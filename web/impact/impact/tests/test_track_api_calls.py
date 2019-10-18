236f88ef973e9a7937269907be74f2267390bd6c
Kevin M Quigley <kquigley@masschallenge.org>
2019-10-18 15:47:43 -0400

    [AC-7058] lint

--- web/impact/impact/tests/test_track_api_calls.py
+++ web/impact/impact/tests/test_track_api_calls.py
@@ -1,19 +1,14 @@
-from django.test import (
-    TestCase,
-)
-
-from mock import mock, patch
+from mock import patch
 
 from impact.tests.api_test_case import APITestCase
 
 
 class TestTrackAPICalls(APITestCase):
-    @patch('impact.middleware.track_api_calls.TrackAPICalls.process_request.logger')
+    @patch('middleware.track_api_calls.TrackAPICalls.process_request.logger')
     def test_when_user_authenticated(self, logger_info_patch):
         with self.login(email=self.basic_user().email):
-            response = self.client.get(/)
-            logger_info_patch.info.assert_called_with()
-
+            self.client.get('/')
+            logger_info_patch.info.assert_called_with({})
 
     def test_when_no_user_authenticated(self):
         pass
