93c51920831491a2ada55dfadf00b1fe7571f3b3
Geofrey Asiimwe <geofrocker2@gmail.com>
2019-10-31 17:51:09 +0300

    [AC-7071] add finalists to v1 permissions

--- web/impact/impact/permissions/v1_api_permissions.py
+++ web/impact/impact/permissions/v1_api_permissions.py
@@ -1,4 +1,5 @@
 from accelerator_abstract.models.base_user_utils import is_employee
+from accelerator_abstract.models.base_user_role import is_finalist_user
 from impact.permissions import (
     settings,
     BasePermission)
@@ -9,4 +10,5 @@ class V1APIPermissions(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.groups.filter(
-            name=settings.V1_API_GROUP).exists() or is_employee(request.user)
+            name=settings.V1_API_GROUP).exists() or is_employee(
+                request.user) or is_finalist_user(request.user)
