2e95306d587ecf6bc943c0cf75a7c99f95a4c960
Geofrey Asiimwe <geofrocker2@gmail.com>
2019-11-01 23:20:09 +0300

    [AC-7071] remove permission classes on endpoint

--- web/impact/impact/permissions/v1_api_permissions.py
+++ web/impact/impact/permissions/v1_api_permissions.py
@@ -1,5 +1,4 @@
 from accelerator_abstract.models.base_user_utils import is_employee
-from accelerator_abstract.models.base_user_role import is_finalist_user
 from impact.permissions import (
     settings,
     BasePermission)
@@ -10,5 +9,4 @@ class V1APIPermissions(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.groups.filter(
-            name=settings.V1_API_GROUP).exists() or is_employee(
-                request.user) or is_finalist_user(request.user)
+            name=settings.V1_API_GROUP).exists() or is_employee(request.user)
