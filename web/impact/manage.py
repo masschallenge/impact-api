93cd0ed39e5b005cca062251145fd579c1fdee31
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-11-06 22:48:10 +0300

    [AC-7180] check for migration exit code

--- web/impact/manage.py
+++ web/impact/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import subprocess
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',
@@ -8,4 +9,9 @@ if __name__ == '__main__':
 
     from configurations.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+    try:
+        execute_from_command_line(sys.argv)
+        subprocess.call("/usr/bin/notify_slack_on_success.sh", shell=True)
+    except SystemExit as exc:
+        # TODO pass the error variable
+        subprocess.call("/usr/bin/notify_slack_on_failure.sh", shell=True)
