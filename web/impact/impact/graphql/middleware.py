8ba9417252b9a2ad8d23bbd7eb80e432d93fa5c3
Paul Bissex <paul@masschallenge.org>
2019-11-14 11:24:20 -0500

    [AC-7025] updated middlware with MiddlewareMixin

--- web/impact/impact/graphql/middleware.py
+++ web/impact/impact/graphql/middleware.py
@@ -1,8 +1,12 @@
+from django.utils.deprecation import MiddlewareMixin
+
 from impact.graphql.utils.response_error import ResponseError
+
+
 NOT_LOGGED_IN_MSG = 'User Not Logged in!'
 
 
-class IsAuthenticatedMiddleware(object):
+class IsAuthenticatedMiddleware(MiddlewareMixin):
     def resolve(self, next, root, info, **args):
         if not info.context.user.is_authenticated():
             raise ResponseError(message=NOT_LOGGED_IN_MSG, code="401")
