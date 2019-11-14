ece2400c0c607121ed26060e75aab05171fe82e6
Paul Bissex <paul@masschallenge.org>
2019-11-14 12:16:15 -0500

    [AC-7025] updated is_authenticated to property

--- web/impact/impact/graphql/middleware.py
+++ web/impact/impact/graphql/middleware.py
@@ -8,6 +8,6 @@ NOT_LOGGED_IN_MSG = 'User Not Logged in!'
 
 class IsAuthenticatedMiddleware(MiddlewareMixin):
     def resolve(self, next, root, info, **args):
-        if not info.context.user.is_authenticated():
+        if not info.context.user.is_authenticated:
             raise ResponseError(message=NOT_LOGGED_IN_MSG, code="401")
         return next(root, info, **args)
