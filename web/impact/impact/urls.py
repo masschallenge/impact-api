f77da8265c8a407ceab216fd6ca9c7d66a354160
Paul Bissex <paul@masschallenge.org>
2019-11-14 12:11:44 -0500

    [AC-7025] fixed sso url include

--- web/impact/impact/urls.py
+++ web/impact/impact/urls.py
@@ -96,7 +96,7 @@ urls = [
     url(r'^api/simpleuser/', include(simpleuser_router.urls)),
     url(r'^api/accelerator/', include(schema_router.urls),
         name='api-root'),
-    url(r'^sso/', include(sso_urlpatterns, namespace="sso")),
+    url(r'^sso/', include(sso_urlpatterns)),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include(account_urlpatterns)),
     url(r'^graphql/$',
