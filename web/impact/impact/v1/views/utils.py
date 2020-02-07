b0d7efb94afe9859243b1f1c4b949a9c3ab05040
Philip Kwesiga <phil.kwesiga@gmail.com>
2020-02-07 12:03:09 +0300

    [AC-6972] Add common functions to maketing views

--- web/impact/impact/v1/views/utils.py
+++ web/impact/impact/v1/views/utils.py
@@ -1,5 +1,11 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
+from future import standard_library
+from django.conf import settings
+from urllib.parse import urlsplit
+
+standard_library.install_aliases()
+PADDING_CHAR = '='
 VALID_KEYS_NOTE = "Valid keys are: {}"
 
 
@@ -24,3 +30,22 @@ def map_data(klass, query, order, data_keys, output_keys):
     data = result.values_list(*data_keys)
     return [dict(zip(output_keys, values))
             for values in data]
+
+def pad(unpadded_str):
+    remainder = len(unpadded_str) % settings.IMAGE_TOKEN_BLOCK_SIZE
+    pad_size = settings.IMAGE_TOKEN_BLOCK_SIZE - remainder
+    padding = pad_size * PADDING_CHAR
+    return unpadded_str + padding
+
+
+def unpad(padded_string):
+    padding_char = padded_string[-1]
+    first_index_of_pad = padded_string.find(padding_char)
+    return padded_string[:first_index_of_pad]
+
+def normalize_url_scheme(url):
+    if not url:
+        return ''
+    if not urlsplit(url).scheme:
+        return "http://" + url
+    return url
