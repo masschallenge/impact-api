d5d96dacd1c2167e0010748682884c05eaa030bb
Philip Kwesiga <phil.kwesiga@gmail.com>
2020-02-07 12:11:07 +0300

    [AC-6972] arrange imports alphabetically

--- web/impact/impact/v1/views/__init__.py
+++ web/impact/impact/v1/views/__init__.py
@@ -47,6 +47,8 @@ from impact.v1.views.judging_round_list_view import (
     INVALID_ROUND_TYPE_ERROR,
     JudgingRoundListView,
 )
+from impact.v1.views.marketing_startup_list_view import MarketingStartupListView
+from impact.v1.views.marketing_startup_profile_view import MarketingStartupProfileView
 from impact.v1.views.organization_detail_view import OrganizationDetailView
 from impact.v1.views.organization_history_view import OrganizationHistoryView
 from impact.v1.views.organization_list_view import OrganizationListView
@@ -58,8 +60,6 @@ from impact.v1.views.program_detail_view import ProgramDetailView
 from impact.v1.views.program_family_detail_view import ProgramFamilyDetailView
 from impact.v1.views.program_family_list_view import ProgramFamilyListView
 from impact.v1.views.program_list_view import ProgramListView
-from impact.v1.views.marketing_startup_list_view import MarketingStartupListView
-from impact.v1.views.marketing_startup_profile_view import MarketingStartupProfileView
 from impact.v1.views.user_confidential_view import UserConfidentialView
 from impact.v1.views.user_detail_view import UserDetailView
 from impact.v1.views.user_history_view import UserHistoryView
