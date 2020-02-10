9688f4b1778f1cc6025b7df5ea2882d356e6f287
Philip Kwesiga <phil.kwesiga@gmail.com>
2020-02-10 15:44:09 +0300

    [AC-6972] add test attributes

---
+++ web/impact/impact/tests/attributes.py
@@ -0,0 +1,5 @@
+from nose.plugins.attrib import attr
+
+now = lambda test: attr('now')(test)
+security1 = lambda test: attr('security1')(test)
+marketingapi = lambda test: attr('marketingapi')(test)
