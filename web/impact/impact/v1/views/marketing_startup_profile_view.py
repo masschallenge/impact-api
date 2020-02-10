38a9e51043d59cd660cbe7af2f43ff38c0214593
Philip Kwesiga <phil.kwesiga@gmail.com>
2020-02-10 09:41:41 +0300

    [AC-6972] minimize profile code into functions

--- web/impact/impact/v1/views/marketing_startup_profile_view.py
+++ web/impact/impact/v1/views/marketing_startup_profile_view.py
@@ -41,8 +41,11 @@ from embed_video.backends import (
 )
 from impact.v1.views.impact_view import ImpactView
 from impact.v1.views.utils import (
-    pad,
+    get_image_token,
     normalize_url_scheme,
+    pad,
+    status_dict,
+    status_displayable,
 )
 
 logger = logging.getLogger('django.request')
@@ -58,41 +61,7 @@ class MarketingStartupProfileView(ImpactView):
         self.acceptable_badge_display = (
             BADGE_STARTUP_PROFILE, BADGE_STARTUP_LIST_AND_PROFILE)
         self.status_groups = set()
-    
-    def get_image_token(self, name):
-        # Create initialization vector,
-        # servicekey and encryption object for creating image_token
-        #  the service key is a hash of the password from the settings file,
-        # we use the first 32 bytes as 32 is
-        #  a key length restriction.
-        iv = os.urandom(settings.IMAGE_TOKEN_BLOCK_SIZE)
-        servicekey = hashlib.sha256(
-            settings.IMAGE_TOKEN_PASSWORD.encode()).hexdigest()[:32]
-        aes = AES.new(servicekey.encode(), AES.MODE_CBC, iv)
-        raw = pad(name + ":" + str(time())).encode()[:32]
-        # We .decode() return value because json.dumps needs str values, not bytes
-        return base64.urlsafe_b64encode((iv + aes.encrypt(raw))).decode()
-
-    def status_dict(self, startup_status):
-        return {
-            'status_name': startup_status.program_startup_status.startup_status,
-            'status_badge_url': (
-                startup_status.program_startup_status.badge_image.url
-            )
-            if startup_status.program_startup_status.badge_image else '',
-            'status_badge_token': self.get_image_token(
-                startup_status.program_startup_status.badge_image.name
-            ) if startup_status.program_startup_status.badge_image else '',
-        }
-    
-    def status_displayable(self, startup_status):
-        pstatus = startup_status.program_startup_status
-        return (
-            not startup_status.program_startup_status.status_group or (
-                startup_status.startup_id,
-                startup_status.program_startup_status.status_group
-            ) not in self.status_groups
-        ) and (pstatus.badge_display in self.acceptable_badge_display)
+        self.data = {}
 
     def website(self, website_url):
         return normalize_url_scheme(website_url)
@@ -109,12 +78,109 @@ class MarketingStartupProfileView(ImpactView):
             logger.warning(VIDEO_LINK_EMBED_MSG.format(str(e), url))
             return ""
 
+    def format_data(self, startup):
+        addition_industry_categories = startup.additional_industries
+        self.data = {
+            "name": startup.name,
+            "is_visible": startup.is_visible,
+            "logo_url": (
+                startup.high_resolution_logo.url
+            ) if startup.high_resolution_logo else "",
+            "image_token": get_image_token(
+                startup.high_resolution_logo.name
+            ) if startup.high_resolution_logo else "",
+            "primary_industry": startup.primary_industry.name,
+            "additional_industries": [
+                industry.name for industry in
+                addition_industry_categories.all()
+            ], "short_pitch": startup.short_pitch,
+            "full_elevator_pitch": startup.full_elevator_pitch,
+            "website_url": self.website(
+                startup.website_url),
+            "twitter_handle": startup.twitter_handle,
+            "public_inquiry_email": startup.public_inquiry_email,
+            "facebook_url": startup.facebook_url,
+            "linked_in_url": startup.linked_in_url,
+            "video_elevator_pitch_url": self.video_link_embed(
+                startup.video_elevator_pitch_url),
+            "statuses": [],
+            "team_members": [],
+            "profile_background_color": "#" + (
+                startup.profile_background_color or
+                DEFAULT_PROFILE_BACKGROUND_COLOR),
+            "profile_text_color": "#" + (
+                startup.profile_text_color or
+                DEFAULT_PROFILE_TEXT_COLOR)
+        }
+
+    def get_statuses(self, startup, program_key):
+        statuses = StartupStatus.objects.order_by(
+            'program_startup_status__sort_order',
+            'program_startup_status__startup_status'). \
+            select_related('program_startup_status').filter(startup=startup)
+        if program_key:
+            statuses.filter(
+                program_startup_status__program__name=program_key
+            )
+        for startup_status in statuses:
+            if status_displayable(startup_status,
+                self.status_groups, self.acceptable_badge_display):
+                self.data['statuses'].append(status_dict(startup_status))
+                self.status_groups.add(
+                    (startup_status.startup_id,
+                    startup_status.program_startup_status.status_group))
+
+    def team_members(self, startup):
+        # If this site has access to team members for a program which the
+        # startup belongs to, fill them in
+        team_members = StartupTeamMember.objects.filter(
+            startup=startup).order_by(
+                'user__last_name', 'user__first_name')
+        team = []
+        for member in team_members:
+            if member.display_on_public_profile:
+                team.append({
+                    'first_name': member.user.first_name,
+                    'last_name': member.user.last_name,
+                    'email': member.user.email,
+                    'title': member.title,
+                    'photo_url': (
+                        member.user.get_profile().image.url
+                    ) if (
+                        member.user.get_profile().image
+                    ) else '',
+                    'photo_token': get_image_token(
+                        member.user.get_profile().image.name
+                    ) if member.user.get_profile().image else '',
+                })
+        self.data.update({'team_members': team})
+
+    def get_non_public(self):
+        self.data = {
+            "name": None,
+            "is_visible": False,
+            "logo_url": "",
+            "image_token": "",
+            "primary_industry": "",
+            "additional_industries": [],
+            "short_pitch": "",
+            "full_elevator_pitch": "",
+            "website_url": "",
+            "twitter_handle": "",
+            "public_inquiry_email": "",
+            "facebook_url": "",
+            "linked_in_url": "",
+            "video_elevator_pitch_url": "",
+            "statuses": [],
+            "team_members": [],
+            "profile_background_color": "",
+            "profile_text_color": "",
+        }
+
     def get(self, request):
-        program_names = request.GET.getlist('program_key')
-        site_name = request.GET['site_name']
         if not request.GET['startup_key']:
             raise ValueError("No startup given.")
-        name = None
+        program_key = request.GET.get('program_key')
         try:
             # contains non-integers
             if re.match(r".*\D.*", request.GET['startup_key']):
@@ -126,109 +192,11 @@ class MarketingStartupProfileView(ImpactView):
         except ObjectDoesNotExist:
             raise PermissionDenied
         is_public = startup.is_visible
-        data = {}
 
         if is_public:
-            addition_industry_categories = startup.additional_industries
-            data = {
-                "name": startup.name,
-                "is_visible": startup.is_visible,
-                "logo_url": (
-                    startup.high_resolution_logo.url
-                ) if startup.high_resolution_logo else "",
-                "image_token": getImageToken(
-                    startup.high_resolution_logo.name
-                ) if startup.high_resolution_logo else "",
-                "primary_industry": startup.primary_industry.name,
-                "additional_industries": [
-                    industry.name for industry in
-                    addition_industry_categories.all()
-                ], "short_pitch": startup.short_pitch,
-                "full_elevator_pitch": startup.full_elevator_pitch,
-                "website_url": self.website(
-                    startup.website_url),
-                "twitter_handle": startup.twitter_handle,
-                "public_inquiry_email": startup.public_inquiry_email,
-                "facebook_url": startup.facebook_url,
-                "linked_in_url": startup.linked_in_url,
-                "video_elevator_pitch_url": self.video_link_embed(
-                    startup.video_elevator_pitch_url),
-                "statuses": [],
-                "team_members": [],
-                "profile_background_color": "#" + (
-                    startup.profile_background_color or
-                    DEFAULT_PROFILE_BACKGROUND_COLOR),
-                # format "#FFFFFF"
-                # format "#FFFFFF"
-                "profile_text_color": "#" + (
-                    startup.profile_text_color or
-                    DEFAULT_PROFILE_TEXT_COLOR)
-            }
-            print(">>>>>>>", data, startup, "\n\n >>>>", type(startup))
-            statuses = StartupStatus.objects.order_by(
-                'program_startup_status__sort_order',
-                'program_startup_status__startup_status'). \
-                select_related('program_startup_status').filter(startup=startup)
-            if request.POST.get('ProgramKey'):
-                statuses.filter(
-                    program_startup_status__program__name=request.POST.get(
-                        'ProgramKey')
-                )
-            for startup_status in statuses:
-                if self.status_displayable(startup_status):
-                    data['statuses'].append(self.status_dict(startup_status))
-                    self.status_groups.add(
-                        (startup_status.startup_id,
-                        startup_status.program_startup_status.status_group))
-
-            # If this site has access to team members for a program which the
-            # startup belongs to, fill them in
-            # 
-            # if Site.is_allowed(
-            #         request.POST.get('SiteName'),
-            #         request.POST.get('SecurityKey'),
-            #         'startup_team_members',
-            #         startup=request.POST.get('StartupKey')):
-            team_members = StartupTeamMember.objects.filter(
-                startup=startup).order_by(
-                    'user__last_name', 'user__first_name')
-            team = []
-            for member in team_members:
-                if member.display_on_public_profile:
-                    team.append({
-                        'first_name': member.user.first_name,
-                        'last_name': member.user.last_name,
-                        'email': member.user.email,
-                        'title': member.title,
-                        'photo_url': (
-                            member.user.get_profile().image.url
-                        ) if (
-                            member.user.get_profile().image
-                        ) else '',
-                        'photo_token': self.get_image_token(
-                            member.user.get_profile().image.name
-                        ) if member.user.get_profile().image else '',
-                    })
-            data.update({'team_members': team})
+            self.format_data(startup)
+            self.get_statuses(startup, program_key)
+            self.team_members(startup)
         else:
-            data = {
-                "name": name,
-                "is_visible": False,
-                "logo_url": "",
-                "image_token": "",
-                "primary_industry": "",
-                "additional_industries": [],
-                "short_pitch": "",
-                "full_elevator_pitch": "",
-                "website_url": "",
-                "twitter_handle": "",
-                "public_inquiry_email": "",
-                "facebook_url": "",
-                "linked_in_url": "",
-                "video_elevator_pitch_url": "",
-                "statuses": [],
-                "team_members": [],
-                "profile_background_color": "",
-                "profile_text_color": "",
-            }
-        return HttpResponse(json.dumps(data), content_type="application/json")
+            self.get_non_public()
+        return HttpResponse(json.dumps(self.data), content_type="application/json")
