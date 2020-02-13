c3a314c9828f7dac641d274e4189c72e6b7995a8
Ian Kigen <ian.Gabe.ian@gmail.com>
2020-02-13 23:33:56 +0300

    [AC-7430] update v1 api permissions

--- web/impact/impact/permissions/v1_api_permissions.py
+++ web/impact/impact/permissions/v1_api_permissions.py
@@ -7,17 +7,19 @@ from impact.permissions import (
 SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
 
 
-def is_object_owner(request, user_id):
-    if user_id and request.method in SAFE_METHODS:
-        return str(user_id) == str(request.user.id)
+def can_view_user_details_page(request):
+    # importing here to avoid circular import
+    from impact.v1.views.user_detail_view import UserDetailView  # noqa: E402
+    if request.resolver_match.url_name == UserDetailView.view_name:
+        user_id = request.parser_context['kwargs'].get('pk', '')
+        if user_id and request.method in SAFE_METHODS:
+            return str(user_id) == str(request.user.id)
 
 
 class V1APIPermissions(BasePermission):
     authenticated_users_only = True
 
     def has_permission(self, request, view):
-        user_id = request.parser_context['kwargs'].get('pk', '')
-
-        return request.user.groups.filter(
-            name=settings.V1_API_GROUP
-        ).exists() or is_employee(request.user) or is_object_owner(request, user_id)
+        return (is_employee(request.user) or
+                can_view_user_details_page(request) or
+                request.user.groups.filter(name=settings.V1_API_GROUP).exists())
