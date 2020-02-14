3f1f8df54db377e7c6e6f17dfae0099dc78dd725
Ian Kigen <ian.Gabe.ian@gmail.com>
2020-02-14 21:09:17 +0300

    [AC-7430] add user details vew permission class

--- web/impact/impact/permissions/v1_api_permissions.py
+++ web/impact/impact/permissions/v1_api_permissions.py
@@ -8,12 +8,9 @@ SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
 
 
 def can_view_user_details_page(request):
-    # importing here to avoid circular import
-    from impact.v1.views.user_detail_view import UserDetailView  # noqa: E402
-    if request.resolver_match.url_name == UserDetailView.view_name:
-        user_id = request.parser_context['kwargs'].get('pk', '')
-        if user_id and request.method in SAFE_METHODS:
-            return str(user_id) == str(request.user.id)
+    user_id = request.parser_context['kwargs'].get('pk', '')
+    if user_id and request.method in SAFE_METHODS:
+        return str(user_id) == str(request.user.id)
 
 
 class V1APIPermissions(BasePermission):
@@ -21,5 +18,10 @@ class V1APIPermissions(BasePermission):
 
     def has_permission(self, request, view):
         return (is_employee(request.user) or
-                can_view_user_details_page(request) or
                 request.user.groups.filter(name=settings.V1_API_GROUP).exists())
+
+
+class UserDetailViewPermission(V1APIPermissions):
+    def has_permission(self, request, view):
+        return (super().has_permission(request, view) or
+                can_view_user_details_page(request))
