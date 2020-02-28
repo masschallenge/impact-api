bd508d2ebaaa80a78f0f417f34071f5726b8e42c
Kevin Quigley <kevinmquigley.41@gmail.com>
2020-02-28 14:02:33 -0500

    [AC-7504] add working test

--- web/impact/impact/tests/test_user_detail_view.py
+++ web/impact/impact/tests/test_user_detail_view.py
@@ -654,6 +654,34 @@ class TestUserDetailView(APITestCase):
             confirmed_program_families = response.data['confirmed_user_program_families']
             assert user_program.program_family.name in confirmed_program_families.keys()
 
+    def test_get_user_with_no_created_at_in_prgs_handled(self):
+        role_grant = ProgramRoleGrantFactory(
+            program_role__program__program_status=ACTIVE_PROGRAM_STATUS,
+            program_role__program__program_family__name="Quigville",
+            created_at=None
+        )
+        user = role_grant.person
+        user_program = role_grant.program_role.program
+
+        role_grant2 = ProgramRoleGrantFactory(
+            person=user,
+            program_role__program__program_status=ACTIVE_PROGRAM_STATUS,
+            program_role__program__program_family__name="Quigville",
+            created_at=None
+        )
+
+        role_grant.created_at = None
+        role_grant2.created_at = None
+
+        role_grant.save()
+        role_grant2.save()
+        
+        with self.login(email=self.basic_user().email):
+            url = reverse(UserDetailView.view_name, args=[user.id])
+            response = self.client.get(url)
+            confirmed_program_families = response.data['confirmed_user_program_families']
+            assert user_program.program_family.name in confirmed_program_families.keys()
+
 
 def _valid_note(messages):
     note_prefix = VALID_KEYS_NOTE.format("")
