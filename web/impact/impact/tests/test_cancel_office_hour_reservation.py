a6ba92023545db988f9e1fad5b77be3e7edd22ec
Jon Kiparsky <jon@kiparsky.net>
2020-04-15 10:03:05 -0400

    [AC-7562] Add tests for mail handler

--- web/impact/impact/tests/test_cancel_office_hour_reservation.py
+++ web/impact/impact/tests/test_cancel_office_hour_reservation.py
@@ -5,7 +5,7 @@ from accelerator.tests.factories import (
 )
 
 from impact.tests.api_test_case import APITestCase
-
+from impact.v1.views import CancelOfficeHourReservationView
 
 class TestCancelOfficeHourReservationView(APITestCase):
     def mentee_cancels_their_own_office_hour_reservation(self):
@@ -23,6 +23,15 @@ class TestCancelOfficeHourReservationView(APITestCase):
         self._submit_cancellation(office_hour, user=office_hour.finalist)
         self.assert_notified(office_hour.mentor)
 
+    def notification_includes_submitted_message(self):
+        cancellation_message = "I'm sorry I had to cancel"
+        office_hour = MentorProgramOfficeHourFactory()
+        self._submit_cancellation(office_hour,
+                                  user=office_hour.finalist,
+                                  message=cancellation_message))
+        self.assert_notified(office_hour.mentor,
+                             message=cancellation_message)
+        
     def staff_user_cancels_their_own_office_hour_reservation(self):
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=self.staff_user())
@@ -59,3 +68,18 @@ class TestCancelOfficeHourReservationView(APITestCase):
         self._submit_cancellation(office_hour, user=self.basic_user())
         self.assert_not_notified(office_hour.mentor)                
         
+
+    def _submit_cancellation(self, office_hour, user, message=""):
+        with self.login(email=user.email):
+            url = reverse(CancelOfficeHourReservationView.view_name)
+            data = {"office_hour_id": office_hour.id,
+                    "message": message}
+            response = self.client.post(url, data=data)
+            return response
+
+    def assert_notified(user, message=""):
+        email = mail.outbox[-1]
+        self.assertContains(email.to, user.email)
+        if message:
+            self.assertContains(email.body, message)
+            
