66ccc77027d956501611bba30f0fc011bdfd53d1
Jon Kiparsky <jon@kiparsky.net>
2020-04-16 14:54:28 -0400

    [AC-7570] Add test for backend gets called

--- web/impact/impact/tests/test_impact_email_backend.py
+++ web/impact/impact/tests/test_impact_email_backend.py
@@ -5,9 +5,32 @@ from django.urls import reverse
 
 from impact.minimal_email_handler import MinimalEmailHandler
 
+
+IMPACT_BACKEND_PATH = 'impact.impact_email_backend.ImpactEmailBackend'
+ADD_LOGGING_HEADERS = ".".join(["impact",
+                                "impact_email_backend",
+                                "ImpactEmailBackend",
+                                "_add_logging_headers"])
+
 class TestEmailBackend(TestCase):
 
-    @patch("impact.impact_email_backend.ImpactEmailBackend._add_logging_headers")
+    @patch(ADD_LOGGING_HEADERS)
+    @patch("django.core.mail.backends.smtp.EmailBackend.send_messages")
+    def test_sending_email_hits_backend(
+        self,
+        mocked_backend,
+        mock_add_logging_headers
+    ):
+        with self.settings(
+                SES_CONFIGURATION_SET="test",
+                EMAIL_BACKEND=IMPACT_BACKEND_PATH):
+            MinimalEmailHandler(["a@example.com"],
+                                "subject",
+                                "body").send()
+            self.assertTrue(mocked_backend.called)
+ 
+
+    @patch(ADD_LOGGING_HEADERS)
     @patch("django.core.mail.backends.smtp.EmailBackend.send_messages")
     def test_email_contains_header_if_ses_config_set(
         self,
@@ -16,13 +39,13 @@ class TestEmailBackend(TestCase):
     ):
         with self.settings(
                 SES_CONFIGURATION_SET="test",
-                EMAIL_BACKEND='mc.email_backends.AccelerateEmailBackend'):
+                EMAIL_BACKEND=IMPACT_BACKEND_PATH):
             MinimalEmailHandler(["a@example.com"],
                                 "subject",
                                 "body").send()
             self.assertTrue(mock_add_logging_headers.called)
-
-    @patch("impact.impact_email_backend.ImpactEmailBackend._add_logging_headers")
+           
+    @patch(ADD_LOGGING_HEADERS)
     @patch("django.core.mail.backends.smtp.EmailBackend.send_messages")
     def test_email_does_not_contain_header_if_ses_config_not_set(
         self,
@@ -31,7 +54,7 @@ class TestEmailBackend(TestCase):
     ):
         with self.settings(
                 SES_CONFIGURATION_SET="",
-                EMAIL_BACKEND='mc.email_backends.AccelerateEmailBackend'):
+                EMAIL_BACKEND=IMPACT_BACKEND_PATH):
             MinimalEmailHandler(["a@example.com"],
                                 "subject",
                                 "body").send()
