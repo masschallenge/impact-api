881524862100c18f25043e176a5c3740ddc9dd83
Jon Kiparsky <jon.kiparsky@gmail.com>
2020-04-21 15:17:39 -0400

    add requested comment

--- web/impact/impact/tests/test_impact_email_backend.py
+++ web/impact/impact/tests/test_impact_email_backend.py
@@ -5,6 +5,11 @@ from impact.minimal_email_handler import MinimalEmailHandler
 
 
 IMPACT_BACKEND_PATH = 'impact.impact_email_backend.ImpactEmailBackend'
+# Note that this definition, which duplicates a value in settings.py, 
+# is necessary since the value is not included in the settings for test
+# See AC-7670 for a ticket aimed at resolving this and other issues 
+# around testing email backends   
+
 ADD_LOGGING_HEADERS = ".".join(["impact",
                                 "impact_email_backend",
                                 "ImpactEmailBackend",
