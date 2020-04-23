249b3ebe766adc35b9c18a6b45c4f6e46ba30555
Ian Kigen <ian.Gabe.ian@gmail.com>
2020-04-23 15:43:03 +0300

    [AC-7642] [AC-7596] add check for program status

--- web/impact/impact/graphql/query.py
+++ web/impact/impact/graphql/query.py
@@ -1,42 +1,21 @@
 import graphene
+from graphql import GraphQLError
 
-from impact.graphql.types import (
-    EntrepreneurProfileType,
-    ExpertProfileType,
-)
 from accelerator.models import (
     EntrepreneurProfile,
-    ExpertProfile,
-    UserRole,
+    ExpertProfile
 )
-from accelerator_abstract.models.base_user_utils import is_employee
-from graphql import GraphQLError
+from impact.graphql.types import (
+    EntrepreneurProfileType,
+    ExpertProfileType
+)
+from impact.permissions.graphql_permissions import (
+    can_view_entrepreneur_profile
+)
 
 ENTREPRENEUR_NOT_FOUND_MESSAGE = 'Entrepreneur matching the id does not exist.'
 EXPERT_NOT_FOUND_MESSAGE = 'Expert matching the id does not exist.'
 NOT_ALLOWED_ACCESS_MESSAGE = 'Sorry, You are not allowed to access this page.'
-
-
-def visible_roles(current_user):
-    basic_user_roles = [
-        UserRole.FINALIST,
-        UserRole.AIR,
-        UserRole.MENTOR,
-        UserRole.PARTNER,
-        UserRole.ALUM
-    ]
-    basic_visible_roles = [UserRole.FINALIST, UserRole.STAFF, UserRole.ALUM]
-
-    current_user_roles = list(
-        current_user.programrolegrant_set.values_list(
-            'program_role__user_role__name', flat=True).distinct()
-    )
-    if not current_user_roles:
-        return [UserRole.STAFF]
-    if set(basic_user_roles).intersection(current_user_roles):
-        return basic_visible_roles + [UserRole.MENTOR]
-    if UserRole.JUDGE in current_user_roles:
-        return basic_visible_roles
 
 
 def has_permission(profile_user, roles):
@@ -66,9 +45,8 @@ class Query(graphene.ObjectType):
         if not entrepreneur:
             return GraphQLError(ENTREPRENEUR_NOT_FOUND_MESSAGE)
 
-        if not is_employee(info.context.user):
-            roles = visible_roles(info.context.user)
-            if not has_permission(entrepreneur.user, roles):
-                return GraphQLError(NOT_ALLOWED_ACCESS_MESSAGE)
+        if not can_view_entrepreneur_profile(info.context.user,
+                                             entrepreneur.user):
+            return GraphQLError(NOT_ALLOWED_ACCESS_MESSAGE)
 
         return entrepreneur
