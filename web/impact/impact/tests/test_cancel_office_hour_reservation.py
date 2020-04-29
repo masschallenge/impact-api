b5008e133c7808e13005df63930c9a38f4346e95
Jon Kiparsky <jon@kiparsky.net>
2020-04-29 00:40:54 -0400

    [AC-7562] lint

--- web/impact/impact/tests/test_cancel_office_hour_reservation.py
+++ web/impact/impact/tests/test_cancel_office_hour_reservation.py
@@ -8,18 +8,23 @@ from accelerator.tests.factories import (
 from impact.tests.api_test_case import APITestCase
 from impact.v1.views import CancelOfficeHourReservationView
 
+
 class TestCancelOfficeHourReservationView(APITestCase):
     def test_mentee_cancels_their_own_office_hour_reservation(self):
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=office_hour.finalist)
         self.assert_reservation_cancelled(office_hour)
 
-    def test_mentee_cancels_office_hour_reservation_and_gets_notification(self):
+    def test_finalist_cancels_and_gets_notification(self):
+        '''A finalist cancelling their reservation should receive a
+        notification'''
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=office_hour.finalist)
         self.assert_notified(office_hour.finalist)
 
-    def test_mentee_cancels_office_hour_reservation_mentor_gets_notification(self):
+    def test_finalist_cancels_mentor_gets_notification(self):
+        '''A finalist cancelling their reservation should trigger a
+        notification to the mentor'''
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=office_hour.finalist)
         self.assert_notified(office_hour.mentor)
@@ -32,43 +37,50 @@ class TestCancelOfficeHourReservationView(APITestCase):
                                   message=cancellation_message)
         self.assert_notified(office_hour.mentor,
                              message=cancellation_message)
-        
-    def test_staff_user_cancels_their_own_office_hour_reservation(self):
+
+    def test_staff_user_cancels_reservation(self):
+        '''A staff user cancelling someone else's office hour should succeed'''
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=self.staff_user())
         self.assert_reservation_cancelled(office_hour)
 
-    def test_staff_user_cancels_office_hour_reservation_and_gets_notification(self):
+    def test_staff_user_trigger_notification_mentor(self):
+        '''A staff user cancelling someone else's office hour should trigger a
+        notification to the mentor'''
+        office_hour = MentorProgramOfficeHourFactory()
+        self._submit_cancellation(office_hour, user=self.staff_user())
+        self.assert_notified(office_hour.mentor)
+
+    def test_staff_user_trigger_notification_finalist(self):
+        '''A staff user cancelling someone else's office hour should trigger a
+        notification to the finalist'''
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=self.staff_user())
         self.assert_notified(office_hour.finalist)
 
-    def test_staff_user_cancels_office_hour_reservation_mentor_gets_notification(self):
-        office_hour = MentorProgramOfficeHourFactory()
-        self._submit_cancellation(office_hour, user=self.staff_user())
-        self.assert_notified(office_hour.mentor)
-        
-    
-    def test_non_staff_user_attempts_to_cancel_reservation_not_theirs(self):
+    def test_non_staff_user_not_finalist_fails(self):
+        '''A non-staff user attempting to cancel someone else's reservation
+        should fail'''
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=self.basic_user())
         self.assert_reservation_not_cancelled(office_hour)
 
-    def test_non_staff_user_attempts_to_cancel_reservation_not_theirs(self):
+    def test_non_staff_user_not_finalist_no_notification(self):
+        '''A non-staff user attempting to cancel someone else's reservation
+        should not receive a notification'''
         office_hour = MentorProgramOfficeHourFactory()
         self._submit_cancellation(office_hour, user=self.basic_user())
-        self.assert_not_notified(office_hour.mentor)                
+        self.assert_not_notified(office_hour.mentor)
 
     def test_user_attempts_to_cancel_non_existent_reservation(self):
         office_hour = MentorProgramOfficeHourFactory(finalist=None)
         self._submit_cancellation(office_hour, user=self.basic_user())
         # assert what?
-        
-    def test_user_attempts_to_cancel_non_existent_reservation_no_notification(self):
+
+    def test_user_cancels_non_existent_reservation_no_notification(self):
         office_hour = MentorProgramOfficeHourFactory(finalist=None)
         self._submit_cancellation(office_hour, user=self.basic_user())
-        self.assert_not_notified(office_hour.mentor)                
-        
+        self.assert_not_notified(office_hour.mentor)
 
     def _submit_cancellation(self, office_hour, user, message=""):
         with self.login(email=self.basic_user().email):
@@ -85,13 +97,20 @@ class TestCancelOfficeHourReservationView(APITestCase):
         self.assertContains(email.to, user.email)
         if message:
             self.assertContains(email.body, message)
-            
+
     def assert_not_notified(self, user):
         if mail.outbox:
             self.assertNotIn(user.email, [email.to for email in mail.outbox],
                              msg="Found an email sent to user")
-        
+
     def assert_reservation_cancelled(self, office_hour):
         office_hour.refresh_from_db()
         self.assertIsNone(office_hour.finalist,
                           msg="Reservation was not cancelled")
+
+    def assert_reservation_not_cancelled(self, office_hour):
+        old_finalist = office_hour.finalist
+        office_hour.refresh_from_db()
+        self.assertEqual(office_hour.finalist,
+                         old_finalist,
+                         msg="Reservation was cancelled")
--- web/impact/impact/v1/views/cancel_office_hour_reservation_view.py
+++ web/impact/impact/v1/views/cancel_office_hour_reservation_view.py
@@ -1,15 +1,18 @@
 from django.contrib.auth import get_user_model
 from rest_framework.response import Response
 
+from impact.minimal_email_handler import MinimalEmailHandler as email_handler
 from impact.v1.views.impact_view import ImpactView
 
 from accelerator_abstract.models.base_user_utils import is_employee
 from accelerator.models import MentorProgramOfficeHour
 
 User = get_user_model()
+
+
 class CancelOfficeHourReservationView(ImpactView):
     view_name = "cancel_office_hours_reservation"
-    
+
     def post(self, request):
         office_hour_id = request.POST.get("office_hour_id")
         message = request.POST.get("message")
@@ -19,27 +22,29 @@ class CancelOfficeHourReservationView(ImpactView):
         success = False
         if _can_cancel(requesting_user, office_hour):
             emails = _prepare_email_notifications(office_hour,
-                                                   message,
-                                                   requesting_user)
+                                                  message,
+                                                  requesting_user)
             _cancel_reservation(office_hour)
             for email in emails:
                 _send_email(email)
             success = True
         return Response({"success": success})
-                 
+
+
 def _can_cancel(user, office_hour):
     return is_employee(user) or user == office_hour.finalist
+
 
 def _cancel_reservation(office_hour):
     office_hour.finalist = None
     office_hour.save()
 
+
 def _send_email(email_details):
     email_handler(**email_details).send()
+
 
 def _prepare_email_notifications(office_hour,
                                  message,
                                  requesting_user):
     return []
-        
-    
