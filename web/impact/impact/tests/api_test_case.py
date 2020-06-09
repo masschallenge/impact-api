65679b649134ae33bdca6e93473160322ef970e9
Jon Kiparsky <jon@kiparsky.net>
2020-06-09 16:53:45 -0400

    [AC-7729] Refactor existing tests to use consistent assertion

--- web/impact/impact/tests/api_test_case.py
+++ web/impact/impact/tests/api_test_case.py
@@ -108,7 +108,11 @@ class APITestCase(TestCase):
             data['detail'] == notification
         ]), msg='Notification data was not as expected')
 
-    def assert_notified(self, user, message=""):
+    def assert_notified(self,
+                        user,
+                        message="",
+                        subject="",
+                        check_alternative=False):
         '''Assert that the user received a notification.
         If `message` is specified, assert that the message appears in one of
         the outgoing emails to this user
@@ -116,11 +120,22 @@ class APITestCase(TestCase):
         emails = [email for email in mail.outbox if user.email in email.to]
         self.assertGreater(len(emails), 0)
         if message:
-            self.assertTrue(any([message in email.body for email in emails]))
-
+            if check_alternative:
+                self.assertTrue(any([_message_included_in_email_alternative(
+                    email, message) for email in emails]))
+            else:
+                self.assertTrue(any([message in email.body for email in emails]))
+        if subject:
+            self.assertIn(subject, [email.subject for email in emails])
+    
     def assert_not_notified(self, user):
         '''Assert that the specified user did not receive a notification.
         '''
         if mail.outbox:
             self.assertNotIn(user.email, [email.to for email in mail.outbox],
                              msg="Found an email sent to user")
+
+
+def _message_included_in_email_alternative(email, message):
+    return any([message in alt[0] for alt in email.alternatives])
+            
