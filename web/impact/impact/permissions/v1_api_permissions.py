65fe6ca1913426627ae2be4cd4347bd8852282ce
Jon Kiparsky <jon@kiparsky.net>
2020-06-12 08:53:31 -0400

    [AC-7729] permissions

--- web/impact/impact/permissions/v1_api_permissions.py
+++ web/impact/impact/permissions/v1_api_permissions.py
@@ -64,9 +64,9 @@ class IsExpertUser(IsAuthenticated):
 
 class ReserveOfficeHourPermission(IsAuthenticated):
     def has_permission(self, request, view):
-        return (super().has_permission(request, view) or
-                is_employee(request.user) or
-                can_reserve_office_hour(request.user))
+        return (super().has_permission(request, view) and
+                (is_employee(request.user) or
+                 can_reserve_office_hour(request.user)))
 
 
 def can_reserve_office_hour(user):
