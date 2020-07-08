98be1eb0ce0ae72fbebe40202e96f10d8d06d8b1
Geofrey Asiimwe <geofrocker2@gmail.com>
2020-07-08 17:59:28 +0300

    [AC-7816] Add utils for calendar links

--- web/impact/impact/v1/views/utils.py
+++ web/impact/impact/v1/views/utils.py
@@ -2,6 +2,9 @@
 # Copyright (c) 2017 MassChallenge, Inc.
 
 from pytz import timezone
+from urllib.parse import urlencode
+from urllib.parse import urlunparse
+from add2cal import Add2Cal
 
 from accelerator.models import (
     ACTIVE_PROGRAM_STATUS,
@@ -9,6 +12,7 @@ from accelerator.models import (
 )
 
 VALID_KEYS_NOTE = "Valid keys are: {}"
+REMINDER_DATE_FORMAT = "%Y%m%dT%H%M%S"
 
 
 def valid_keys_note(keys):
@@ -54,3 +58,96 @@ def is_office_hour_reserver(user):
     return user.programrolegrant_set.filter(
         program_role__user_role__name__in=reserver_roles,
         program_role__program__program_status=ACTIVE_PROGRAM_STATUS).exists()
+
+def _get_reminder_params(office_hour, recipient='finalist'):
+    start_datetime = office_hour.start_date_time
+    end_datetime = office_hour.end_date_time
+    tz = office_hour.get_timezone()
+    start_date = start_datetime.astimezone(tz).strftime(REMINDER_DATE_FORMAT)
+    end_date = end_datetime.astimezone(tz).strftime(REMINDER_DATE_FORMAT)
+    topics_block = ""
+    info_block = ""
+
+    if recipient == 'mentor':
+        title = "Office hour with %s %s" % (
+            office_hour.finalist.first_name,
+            office_hour.finalist.last_name)
+    else:
+        title = "Office hour with %s %s" % (
+            office_hour.mentor.first_name,
+            office_hour.mentor.last_name)
+
+    if office_hour.topics:
+        topics_block = "Topics: {topics}".format(
+            topics=office_hour.topics)
+
+    if office_hour.meeting_info:
+        info_block = "Meeting info: {info}".format(
+            info=office_hour.meeting_info)
+
+    description = """
+    {description}
+
+    {topics}
+
+    {info}
+    """.format(
+        description=office_hour.description,
+        topics=topics_block,
+        info=info_block)
+
+    return {
+        'start': start_date,
+        'end': end_date,
+        'title': title,
+        'description': description,
+        'location': office_hour.location_name,
+        'timezone': tz
+    }
+
+
+def generate_ical_content(office_hour):
+    params = _get_reminder_params(office_hour)
+    cal = Add2Cal(**params)
+    return cal.ical_content()
+
+
+def generate_reminder_link(
+        office_hour,
+        link_type='ical',
+        recipient='finalist'):
+    params = _get_reminder_params(office_hour, recipient=recipient)
+    params['link_type'] = link_type
+    host = settings.IMPACT_HOST
+    return _build_url(host, params)
+
+
+def generate_calendar_links(hour, recipient='finalist'):
+    return {
+        'yahoo_reminder': generate_reminder_link(
+            hour,
+            link_type='yahoo',
+            recipient=recipient),
+        'google_reminder': generate_reminder_link(
+            hour,
+            link_type='google',
+            recipient=recipient),
+        'ical_reminder': generate_reminder_link(
+            hour,
+            recipient=recipient),
+        'outlook_reminder': generate_reminder_link(
+            hour,
+            link_type='outlook',
+            recipient=recipient),
+    }
+
+
+def _build_url(baseurl, args_dict):
+
+    url_parts = list(urlparse('//' + baseurl + '/api/calendar/reminder/'))
+    if args_dict.get('link_type') == 'ical':
+        url_parts[0] = 'webcal'
+    else:
+        url_parts[0] = 'https'
+    url_parts[4] = urlencode(args_dict)
+    return urlunparse(url_parts)
