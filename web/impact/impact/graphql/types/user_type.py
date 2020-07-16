b713c789d7d6707e733e76818ca39d76503a9d9c
Ian Kigen <ian.Gabe.ian@gmail.com>
2020-07-16 20:04:11 +0300

    [AC-7820] include privacy check for phone, web and email

--- web/impact/impact/graphql/types/user_type.py
+++ web/impact/impact/graphql/types/user_type.py
@@ -1,6 +1,8 @@
+from accelerator.models import ExpertProfile
+from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
 
-from django.contrib.auth import get_user_model
+from ...graphql.auth.utils import can_view_private_data
 
 User = get_user_model()
 
@@ -9,3 +11,11 @@ class UserType(DjangoObjectType):
     class Meta:
         model = User
         only_fields = ('id', 'first_name', 'last_name', 'email')
+
+    def resolve_email(self, info, **kwargs):
+        profile = self.get_profile()
+        email = self.email
+        if type(profile) is ExpertProfile:
+            if not can_view_private_data(info.context.user, profile.privacy_email):
+                email = ""
+        return email
