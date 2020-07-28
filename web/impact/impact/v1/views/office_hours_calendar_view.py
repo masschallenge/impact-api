986ee51a0a94e01e8c7a5d08bea9aee1e1a7e866
Ian Kigen <ian.Gabe.ian@gmail.com>
2020-07-28 20:26:06 +0300

    [AC-7914] exclude null locations from location choices list

--- web/impact/impact/v1/views/office_hours_calendar_view.py
+++ web/impact/impact/v1/views/office_hours_calendar_view.py
@@ -215,6 +215,7 @@ class OfficeHoursCalendarView(ImpactView):
                                              "program",
                                              "program_family",
                                              "name"])
+        self.not_null_location = Q(**{self.location_path + '__isnull': False})
         return True
 
     def _get_office_hours_data(self):
@@ -253,7 +254,6 @@ class OfficeHoursCalendarView(ImpactView):
             finalist_email=F("finalist__email"),
             mentor_email=F("mentor__email"),
         )
-        self.response_elements['location_choices'] = self.location_choices()
         self.response_elements['timezones'] = office_hours.filter(
             location__isnull=False).order_by(
                 "location__timezone").values_list("location__timezone",
@@ -276,8 +276,9 @@ class OfficeHoursCalendarView(ImpactView):
             self.program_family, flat=True).distinct()
 
     def location_choices(self):
-        locations = self.user_query.values(
-            **_location_lookups(self.location_path)).distinct()
+        locations = self.user_query.filter(
+            self.not_null_location
+        ).values(**_location_lookups(self.location_path)).distinct()
         locations_list = list(locations)
         has_remote_location = _check_remote_location(locations_list)
 
