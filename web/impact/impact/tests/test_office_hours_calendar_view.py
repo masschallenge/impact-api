1ebd38e4520ca82e2b45fb4240bc229382cf70ec
Paul Bissex <paul@masschallenge.org>
2020-09-18 11:04:22 -0400

    [AC-7887] added whitespace after commas

--- web/impact/impact/tests/test_office_hours_calendar_view.py
+++ web/impact/impact/tests/test_office_hours_calendar_view.py
@@ -66,8 +66,8 @@ class TestOfficeHoursCalendarView(APITestCase):
         self.assert_hour_in_response(response, office_hour)
 
     def test_calendar_data_for_month_span_last_day_of_the_month(self):
-        end_of_month = datetime(2020,1,31,5,0,0)
-        focal_day  = datetime(2020,1,3,5,0,0)
+        end_of_month = datetime(2020, 1, 31, 5, 0, 0)
+        focal_day  = datetime(2020, 1, 3, 5, 0, 0)
         office_hour = self.create_office_hour(start_date_time=end_of_month)
         focal_date = focal_day.strftime(ISO_8601_DATE_FORMAT)
         response = self.get_response(user=office_hour.mentor,
@@ -76,8 +76,8 @@ class TestOfficeHoursCalendarView(APITestCase):
         self.assert_hour_in_response(response, office_hour)
 
     def test_calendar_data_for_month_span_mid_month(self):
-        end_of_month = datetime(2020,1,31,5,0,0)
-        mid_month = datetime(2020,1,15,5,0,0)
+        end_of_month = datetime(2020, 1, 31, 5, 0, 0)
+        mid_month = datetime(2020, 1, 15, 5, 0, 0)
         office_hour = self.create_office_hour(start_date_time=mid_month)
         focal_date = end_of_month.strftime(ISO_8601_DATE_FORMAT)
         response = self.get_response(user=office_hour.mentor,
