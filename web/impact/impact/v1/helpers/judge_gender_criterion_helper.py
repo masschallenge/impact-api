711f138b7554e94475dace73a0887c723f0001d4
Geofrey Asiimwe <geofrocker2@gmail.com>
2021-02-09 19:35:49 +0300

    [AC-8374] update judge field to match the new gender field

--- web/impact/impact/v1/helpers/judge_gender_criterion_helper.py
+++ web/impact/impact/v1/helpers/judge_gender_criterion_helper.py
@@ -1,12 +1,14 @@
 # MIT License
 # Copyright (c) 2017 MassChallenge, Inc.
-
-from impact.v1.helpers.judge_criterion_helper import JudgeCriterionHelper
 from django.db.models import F
+from collections import defaultdict
+
+from mc_apply.reports.concat import Concat
+from .judge_criterion_helper import JudgeCriterionHelper
 
 
 class JudgeGenderCriterionHelper(JudgeCriterionHelper):
-    cache_judge_field = "expertprofile__gender"
+    cache_judge_field = "expertprofile__gender_identity__name"
     judge_field = "judge__" + cache_judge_field
     cache_key = cache_judge_field
 
@@ -17,6 +19,11 @@ class JudgeGenderCriterionHelper(JudgeCriterionHelper):
         }
 
     def analysis_tally(self, app_id, db_value, cache, **kwargs):
+        gender_dict = defaultdict(lambda: "o")
+        gender_dict.update({"Female": 'f',"Male": 'm'})
+        db_value["gender"] = gender_dict[db_value["gender"]]
+        db_value[self.cache_judge_field] = db_value["gender"]
+        db_value[self.judge_field] = db_value["gender"]
         gender_value = cache[app_id]["gender"].get(
             db_value["gender"])
         cache[app_id]["gender"][db_value["gender"]] = (
